655552ade87f6ba0db956eb3e58e54e43f778f0e
annmulwa <annmulwa2000@gmail.com>
2024-08-01 14:28:05 +0300

    personal_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -48,6 +48,21 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     )
 
 
+def main():
+    """
+    the main function
+    """
+    con = get_db()
+    users = con.cursor()
+    users.execute("SELECT CONCAT('name=', name, ';ssn=', ssn, ';ip=', ip, \
+        ';user_agent', user_agent, ';') AS message FROM users;")
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    logger = get_logger()
+
+    for user in users:
+        logger.log(logging.INFO, user[0])
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
