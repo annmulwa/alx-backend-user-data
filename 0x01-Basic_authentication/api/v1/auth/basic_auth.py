53073d159f4e80fa14dedf7acbdc358e19efcd27
annmulwa <annmulwa2000@gmail.com>
2024-08-06 14:55:54 +0300

    basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,11 +2,27 @@
 """
 Basic authentication module for the API.
 """
+import re
+import base64
+import binascii
+from typing import Tuple, TypeVar
 from .auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
     """
     Basic authentication class.
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the Authorization
+        header for a Basic Authentication
+        """
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
