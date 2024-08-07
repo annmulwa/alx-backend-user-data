0e4355cacaf04a80e17f9eb9a63fcb44fa2f19a0
annmulwa <annmulwa2000@gmail.com>
2024-08-07 16:53:51 +0300

    Session Authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,7 +3,9 @@
 Session authentication module for the API.
 """
 from uuid import uuid4
+from flask import request
 from .auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -21,3 +23,16 @@ class SessionAuth(Auth):
             self.user_id_by_session_id[session_id] = user_id
             return session_id
 
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns a User ID based on a Session ID
+        """
+        if type(session_id) is str:
+            return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> User:
+        """
+        Returns a User instance based on a cookie value
+        """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
