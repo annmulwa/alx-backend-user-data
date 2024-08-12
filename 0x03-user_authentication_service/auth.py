048c761e4d0054e17230467e05054689ed35a156
annmulwa <annmulwa2000@gmail.com>
2024-08-12 17:47:30 +0300

    user authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,7 +5,7 @@ Auth Module
 
 from db import DB
 from user import User
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from sqlalchemy.orm.exc import NoResultFound
 
 
@@ -34,3 +34,14 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Expect email and password required
+        arguments and return a boolean.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return checkpw(password.encode('utf-8'), user.hashed_password)
