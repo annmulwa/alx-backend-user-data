edd5fdcfd66d45f2b1da598eab75581ba8554889
annmulwa <annmulwa2000@gmail.com>
2024-08-12 16:30:31 +0300

    user authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,8 +3,10 @@
 Flask App Module
 """
 
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -14,6 +16,20 @@ def home() -> str:
     Home Page
     """
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """
+    End-point to register a user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": "f'{email}", "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
