198cff5b0379567033649ea072168a3571b70808
annmulwa <annmulwa2000@gmail.com>
2024-08-13 14:59:21 +0300

    user authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -89,5 +89,21 @@ def get_reset_password_token() -> str:
         return jsonify({"email": f"{email}", "reset_token": f"{token}"})
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """
+    Respond to the PUT /reset_password route
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": f"{email}",
+                        "message": "Password updated"}), 200
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
